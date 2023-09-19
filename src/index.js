5c0f52cbf06eb14e0b34f2d684d08e024a154c9f
Pereselakilya <104826618+PereselakIlya@users.noreply.github.com>
2023-09-19 11:37:49 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var counter=0;
+  bracketsConfig=bracketsConfig.flat();
+  for(var i=0;i<bracketsConfig.length;++i)
+  {
+    if(bracketsConfig.length==2){
+      if(str.includes(bracketsConfig[i]))
+      {
+        counter+=1;
+      }  
+    }
+    else{
+      if(str.includes(bracketsConfig[i]+bracketsConfig[i+1])){
+        counter+=1;
+      }
+      else{
+        counter-=1;
+      }
+    }
+  }
+  if(counter>0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
